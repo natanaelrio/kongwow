e3f2663ac6148bf5729aaac0cbbce7490efd16c8
Natanael Rio Wijaya <gafterlife@gmail.com>
2025-01-18 15:57:00 +0700

    rio

--- src/component/googleMap.jsx
+++ src/component/googleMap.jsx
@@ -37,7 +37,7 @@ export default function GoogleMap() {
                 </div>
 
                 <div className={styles.googlemap}>
-                    <iframe className={styles.dalamgooglemap} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7468.877208383998!2d110.48159965481007!3d-7.4097903519187645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a7b11ab668a95%3A0xe5cea7fd5efd33b4!2sKong%20Wow%20%7C%20Camilan%20Singkong%20Istimewa!5e0!3m2!1sen!2sid!4v1737174767395!5m2!1sen!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe className={styles.dalamgooglemap} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7468.877208383998!2d110.48159965481007!3d-7.4097903519187645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a7b11ab668a95%3A0xe5cea7fd5efd33b4!2sKong%20Wow%20%7C%20Camilan%20Singkong%20Istimewa!5e0!3m2!1sen!2sid!4v1737174767395!5m2!1sen!2sid" loading="lazy"></iframe>
                 </div>
             </div>
         </div >
