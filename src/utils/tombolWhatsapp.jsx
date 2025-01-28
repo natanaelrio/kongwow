bc04ce2a0bec553c1b701feef5f64c9ea1797bea
Natanael Rio Wijaya <gafterlife@gmail.com>
2025-01-28 22:43:13 +0700

    rio

--- src/utils/tombolWhatsapp.jsx
+++ src/utils/tombolWhatsapp.jsx
@@ -2,7 +2,7 @@
 import { sendGTMEvent } from '@next/third-parties/google'
 
 export const OpenWhatsApp = () => {
-    sendGTMEvent({ event: 'buttonClicked', value: 'xyz' })
+    sendGTMEvent({ event: 'buttonClicked' })
     // const encodedMessage = encodeURIComponent(`Halo, saya tertarik dengan produk camilan Singkong Kongwow. Bisa minta daftar produknya?`);
     // const whatsappNumber = process.env.NEXT_PUBLIC_WA; // Ganti dengan nomor WhatsApp Anda (dalam format internasional, tanpa "+" atau "00")
     // const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodedMessage}`;
